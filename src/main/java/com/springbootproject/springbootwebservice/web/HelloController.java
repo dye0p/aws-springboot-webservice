4a55f706a24e7bd007dfe3d9e0d40cd246a03b97
DongYeop <s01054830455@gmail.com>
2024-04-23 17:11:53 +0900

    add: dto 컨트롤러, test 추가

--- src/main/java/com/springbootproject/springbootwebservice/web/HelloController.java
+++ src/main/java/com/springbootproject/springbootwebservice/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.springbootproject.springbootwebservice.web;
 
+import com.springbootproject.springbootwebservice.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") //@RequestParam : 외부에서 "name"이란 이름으로 넘긴 파라미터를 가져옴
+                                     String name, //name 변수에 저장
+                                     @RequestParam("amount")
+                                     int amount) {
+        return new HelloResponseDto(name, amount); //받은 값을 생성자로 하여 HelloResponseDto 객체를 새로 생성하여 리턴
     }
 }
--- src/test/java/com/springbootproject/springbootwebservice/web/HelloControllerTest.java
+++ src/test/java/com/springbootproject/springbootwebservice/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class) //Junit5 테스트 어노테이션
 @WebMvcTest(controllers = HelloController.class) //MVC 컨트롤러 테스트
@@ -25,5 +25,23 @@ class HelloControllerTest {
         mvc.perform(get("/hello")) // '/hello' 주소로 HTTP GET 요청
                 .andExpect(status().isOk()) //HTTP 응답이 200인지 확인
                 .andExpect(content().string(hello)); //응답의 본문이 "hello"인지 확인
+    }
+
+    @Test
+    void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name) //API 테스트시 사용될 요청 파라미터 설정, String값만 허용
+                                .param("amount", String.valueOf(amount))) //int형을 String형으로 변환
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+        /*
+        jsonPath : JSON 응답값을 필드별로 검증할 수 있는 메서드
+                   $를 기준으로 필드명 명시
+        */
     }
 }
