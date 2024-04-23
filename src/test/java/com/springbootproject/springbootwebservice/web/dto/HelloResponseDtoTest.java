3ebb7c040c94c56eca4b39999f450634ace214c4
DongYeop <s01054830455@gmail.com>
2024-04-23 16:45:55 +0900

    test: 롬복 사용 HelloResponseDtoTest 추가

---
+++ src/test/java/com/springbootproject/springbootwebservice/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,26 @@
+package com.springbootproject.springbootwebservice.web.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HelloResponseDtoTest {
+
+    @Test
+    void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+        /*
+        isEqualTo : 동등 비교 메서드, assertThat에 있는 값과 비교하여 같은경우 성공
+        * */
+
+    }
+}
