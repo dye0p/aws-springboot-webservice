30a6fc7a87ac9515eeb0da34ce77fbfcc7593512
DongYeop <s01054830455@gmail.com>
2024-04-24 15:13:27 +0900

    test: PostsRepositoryTest 테스트 코드 추가

---
+++ src/test/java/com/springbootproject/springbootwebservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.springbootproject.springbootwebservice.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach //Junit 단계에서 테스트가 끝날 떄마다 수행되는 메서드 지정, Junit4의 @After와 동일
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("dlehdduq0455@naver.com")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
