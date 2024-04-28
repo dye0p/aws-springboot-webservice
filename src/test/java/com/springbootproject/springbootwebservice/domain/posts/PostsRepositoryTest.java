374fad9ec6d622ce594c44e9c90a43f03dc72998
DongYeop <s01054830455@gmail.com>
2024-04-29 01:18:20 +0900

    test: JPA Auditing 테스트

--- src/test/java/com/springbootproject/springbootwebservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/springbootproject/springbootwebservice/domain/posts/PostsRepositoryTest.java
@@ -1,12 +1,14 @@
 package com.springbootproject.springbootwebservice.domain.posts;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,5 +43,26 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    @DisplayName("시간 등록 테스트")
+    void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2024, 4, 29, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>> createdDate=" + posts.getCreatedDate()+",modifiedDate = "+ posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
