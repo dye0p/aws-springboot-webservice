11154802b1461e6a4c5f1e8ad2f2f5e33844031e
DongYeop <s01054830455@gmail.com>
2024-05-10 19:51:19 +0900

    feat: 구글 로그인 버튼 구현 + 컨트롤러 추가

--- src/main/java/com/springbootproject/springbootwebservice/web/IndexController.java
+++ src/main/java/com/springbootproject/springbootwebservice/web/IndexController.java
@@ -1,7 +1,9 @@
 package com.springbootproject.springbootwebservice.web;
 
+import com.springbootproject.springbootwebservice.domain.config.auth.dto.SessionUser;
 import com.springbootproject.springbootwebservice.service.PostsService;
 import com.springbootproject.springbootwebservice.web.dto.PostsResponseDto;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,11 +15,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     //홈
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDese());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
